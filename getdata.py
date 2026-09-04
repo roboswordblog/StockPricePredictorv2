c15f01cadab2f4f693de032d9eedee214fef2942
Robosword <robotanmol@gmail.com>
2026-09-04 20:51:45 +0000

    get all of the data

--- getdata.py
+++ getdata.py
@@ -1,7 +1,7 @@
 import yfinance as yf
 
 data = yf.download("AAPL", start="2014-01-01")
-data.to_csv("data/aapl.csv")
+data.to_csv("data/APPL.csv")
 
 data = yf.download("GOOGL", start="2014-01-01")
 data.to_csv("data/google.csv")
@@ -11,3 +11,6 @@ data.to_csv("data/MSFT.csv")
 
 data = yf.download("AMZN", start="2014-01-01")
 data.to_csv("data/AMZN.csv")
+
+data = yf.download("NVDA", start="2020-01-01")
+data.to_csv("aapl.csv")
